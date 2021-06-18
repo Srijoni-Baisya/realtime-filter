c01355655ef2a9cbb17fbcd693d064e00c530fb5
Srijoni-Baisya <75311639+Srijoni-Baisya@users.noreply.github.com>
2021-06-18 18:46:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+    //add clown img link
+}
+
+function setup(){
+    canvas = createCanvas(400,400);
+    canvas.center();//store canvas in var and place it in center
+}
+
+function draw(){
+    
+}
+
+function take_snapshot(){
+    save('FilteredClownNoseImage.png');//save snapshot on clicking button 
+}
